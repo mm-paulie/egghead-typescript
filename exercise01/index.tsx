5776018f6911036be0c988951b6a361ca5400a81
Paulie <paulie@mediamonks.com>
2022-09-09 11:44:58 +0200

    exercise01

---
+++ exercise01/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+// const App = () => <div>Hi</div>;
+
+class App extends React.Component {
+  render() {
+    return <div>Hi</div>;
+  }
+}
+
+render(<App />, document.getElementById("app"));
